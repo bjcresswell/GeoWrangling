57e56dfeb9135b242aa84eb8b7216fd06df4d0b3
Ben Cresswell <benjamin.cresswell@jcu.edu.au>
2022-05-17 07:17:18 +1000

    Added CSMP site wrangling script

--- code/rovlog_diw.R
+++ code/rovlog_diw.R
@@ -6,6 +6,15 @@ library(parzer)
 library(readxl)
 library(tidyverse)
 
+getwd()
+
+rovlog <- read_excel('data/ROV Field Log Master.xlsx', 1, trim_ws = TRUE)
+
+rov_geo <- rovlog %>% 
+  select(Trip, Reef, Site, R_site_code, LAT, LONG) %>% 
+  #mutate(Trip = as.Date(Trip, '%m/%y', origin = "1900-01-01")) %>% 
+  mutate_if(is_character, as_factor) %>% 
+  distinct()
 
 
-rovlog <- read_excel()
+
